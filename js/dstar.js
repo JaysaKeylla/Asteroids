cb14343ff2c421661e5426139dd35cba4839d680
jaysa <jaysa@ufrn.edu.br>
2018-11-09 08:07:30 -0300

    Mudando nave

--- js/dstar.js
+++ js/dstar.js
@@ -37,8 +37,7 @@ function Dstar() {
         push();
         translate(this.pos.x, this.pos.y);
         rotate(this.heading + PI / 2);
-        fill(0);
-        stroke(255);
+        
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
         pop();
     }
