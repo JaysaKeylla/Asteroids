8fe5d518adc6a3e81432d6b324ac16e725808500
jaysa <jaysa@ufrn.edu.br>
2018-11-13 09:48:23 -0300

    Cor da nave

--- js/dstar.js
+++ js/dstar.js
@@ -37,7 +37,7 @@ function Dstar() {
         push();
         translate(this.pos.x, this.pos.y);
         rotate(this.heading + PI / 2);
-        fill(100);
+        fill(75);
         stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
         pop();
